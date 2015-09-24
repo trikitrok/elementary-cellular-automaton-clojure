1918827c9d716905fd92cd527ebefde769784cf5
trikitrok <trikitrok@gmail.com>
2015-09-24 15:26:13 +0200

    Simplified tests

--- test/elementary_cellular_automaton/core_test.clj
+++ test/elementary_cellular_automaton/core_test.clj
@@ -2,19 +2,20 @@
   (:use midje.sweet)
   (:use [elementary-cellular-automaton.core]))
 
-(facts
-  "about an elementary cellular automaton"
+(def rule-30 {[1 1 1] 0
+              [1 1 0] 0
+              [1 0 1] 0
+              [1 0 0] 1
+              [0 1 1] 1
+              [0 1 0] 1
+              [0 0 1] 1
+              [0 0 0] 0})
 
-  (facts
-    "about producing the next generation for a given rule"
-    (let [rule-30 {[1 1 1] 0
-                   [1 1 0] 0
-                   [1 0 1] 0
-                   [1 0 0] 1
-                   [0 1 1] 1
-                   [0 1 0] 1
-                   [0 0 1] 1
-                   [0 0 0] 0}]
-      (next-generation [1] rule-30) => [1 1 1]
-      (next-generation [1 1 1] rule-30) => [1 1 0 0 1]
-      (next-generation [1 1 0 0 1] rule-30) => [1 1 0 1 1 1 1])))
+(facts
+  "An elementary cellular automaton"
+
+  (fact
+    "produces the next generation for rule 30"
+    (next-generation [1] rule-30) => [1 1 1]
+    (next-generation [1 1 1] rule-30) => [1 1 0 0 1]
+    (next-generation [1 1 0 0 1] rule-30) => [1 1 0 1 1 1 1]))
